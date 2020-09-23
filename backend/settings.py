45780102ed9aec1ce1cc4f30cd433c7b0cf86857
Micah <mcmanyika@gmail.com>
2020-09-23 17:07:47 -0500

    static file

--- backend/settings.py
+++ backend/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static', 'static_dirs'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
 VENV_PATH = os.path.dirname(BASE_DIR)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static_root')
 MEDIA_ROOT = os.path.join(VENV_PATH, 'static', 'image')
